1d20b66ccf87100ff5df6a3d731ad654fa86ebcd
3pls0de <43237480+3pls0de@users.noreply.github.com>
2021-10-14 14:48:22 +0700

    Create delete.php

---
+++ bab13/Latihan/delete.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once("connection.php");
+
+if (isset($_GET['nis'])) {
+	$nis = $_GET['nis'];
+}else{
+	echo "NIS Tidak ditemukan! <a href='index.php'>Kembali</a>";
+	exit();
+}
+
+$query = "DELETE FROM siswa WHERE nis = '{$nis}'";
+$result = mysqli_query($mysqli, $query);
+
+if (!result) {
+	exit("Gagal menghapus data!");
+}
+header("Location: index.php");
+
+?>
