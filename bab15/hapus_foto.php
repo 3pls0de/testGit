739ac7902b7adcb0827af14b2bcab2eb665ce7ca
3pls0de <43237480+3pls0de@users.noreply.github.com>
2021-10-15 08:47:35 +0700

    Create hapus_foto.php

---
+++ bab15/hapus_foto.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once("connection.php");
+
+if (isset($_GET['id_barang'])) $id_barang = $_GET['id_barang'];
+else{
+	echo "ID Barang Tidak ditemukan! <a href='index.php'>Kembali</a>";
+	exit();
+}
+
+$query = "SELECT * FROM barang WHERE id_barang = '{$id_barang}'";
+
+$result = mysqli_query($mysqli, $query);
+
+foreach ($result as $barang) {
+	$foto = $barang["foto"];
+}
+
+if (!is_null($foto) && !empty($foto)) {
+	$remove = unlink($foto);
+
+	if ($remove) {
+		$query = "UPDATE barang SET foto = NULL WHERE id_barang = '{$id_barang}'";
+		$insert = mysqli_query($mysqli, $query);
+	}
+}
+
+header("Location: form_edit.php?id_barang={$id_barang}");
+
+?>
