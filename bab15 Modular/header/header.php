edf2af69bf54689917c047c1b29b87ea8067fa0a
3pls0de <43237480+3pls0de@users.noreply.github.com>
2021-10-17 13:26:57 +0700

    Create header.php

---
+++ bab15 Modular/header/header.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="container-fluid">
+			<!-- Navbar-brand -->
+			<a href="#" class="navbar-brand">
+				<img src="http://jongkreatif.id/_nuxt/img/logo.825423d.png"/>
+			</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<!-- Navbar Collapse -->
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+					<li class="nav-item">
+						<a href="index.php" class="nav-link" aria-current="page">Daftar Barang</a>
+					</li>
+					<?php
+
+					if ($sessionStatus == false) {
+
+					?>
+
+					<li class="nav-item">
+						<a href="login.php" class="nav-link" aria-current="page">Login Petugas</a>
+					</li>
+
+					<?php } else { ?>
+
+					<li class="nav-item">
+						<a href="logout.php" class="nav-link" aria-current="page">LogOut <?=$sessionName?></a>
+					</li>
+
+					<?php } ?>
+
+				</ul>
+			</div>
+		</div>
+	</nav>
