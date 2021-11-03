e91f461a81920d1fd060f0f7aa88642dd0e00519
3pls0de <43237480+3pls0de@users.noreply.github.com>
2021-11-03 15:01:48 +0700

    apasaja

--- project-bumdesa-darussalam/kreasi/index.php
+++ project-bumdesa-darussalam/kreasi/index.php
@@ -77,6 +77,8 @@
 	        </div>
 	        <div>
 	  				<p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country. But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.</p>
+	  				<a href="about.php">go to about page</a>
+<!-- 	  			</br> -->
 	  			</div>
   			</div>
     	</div>
@@ -214,7 +216,7 @@
 
 
 
-    <section class="ftco-section img" id="ftco-testimony" style="background-image: url(images/bg_1.jpg);"  data-stellar-background-ratio="0.5">
+    <!-- <section class="ftco-section img" id="ftco-testimony" style="background-image: url(images/bg_1.jpg);"  data-stellar-background-ratio="0.5">
     	<div class="overlay"></div>
 	    <div class="container">
 	      <div class="row justify-content-center mb-5">
@@ -294,7 +296,7 @@
 	        </div>
 	      </div>
 	    </div>
-	  </section>
+	  </section> -->
 
     <?php require_once('komponen/footer.php'); ?>
     
